2909057471860b904d6c0ebb15792977027ae46e
revemu <128660623+revemu@users.noreply.github.com>
2025-08-20 19:56:37 +0700

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -62,7 +62,7 @@ async function process_cmd(cmd_str, member) {
         case 'topscorer':
             msg = await db.getTopStat(10, 0) ;
             //console.log(msg) ;
-            content = content.replace(/(\r\n|\n|\r)/gm, "");
+            //content = content.replace(/(\r\n|\n|\r)/gm, "");
             //console.log(content) ;
             const data = {
                 img_url: 'https://static.vecteezy.com/system/resources/thumbnails/028/142/355/small_2x/a-stadium-filled-with-excited-fans-a-football-field-in-the-foreground-background-with-empty-space-for-text-photo.jpg',
