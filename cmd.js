2489c78a9b273c65d5adad9a9ba05acc0ed6f91e
revemu <128660623+revemu@users.noreply.github.com>
2025-08-20 08:51:22 +0700

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -2,11 +2,13 @@ const db = require('./query');
 
 async function process_cmd(cmd_str) {
     const pos = cmd_str.indexOf(" ") ;
+    let cmd ;
+    let param ;
     if (pos > 0) {
-        const cmd = cmd_str.substring(0,pos).trim() ;
-        const param = cmd_str.substring(pos).trim() ;
+        cmd = cmd_str.substring(0,pos).trim() ;
+        param = cmd_str.substring(pos).trim() ;
     } else {
-        const cmd = cmd_str.trim() ;
+        cmd = cmd_str.trim() ;
     }
     
     console.log(`${cmd} - ${param}`) ;
