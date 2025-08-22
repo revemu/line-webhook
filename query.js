c4842a6ca06b985978c4e27532634d42e098e2df
revemu <128660623+revemu@users.noreply.github.com>
2025-08-22 18:44:37 +0700

    Update query.js

--- query.js
+++ query.js
@@ -125,7 +125,7 @@ async function queryMatchGoal(match_id, goal_status = 0) {
   }
   query = `SELECT member_tbl.name, member_tbl.alias, goal_status_tbl.status,match_goal_tbl.status as statusid, count(*) as goal FROM match_goal_tbl, member_tbl, goal_status_tbl WHERE match_goal_tbl.match_id=${match_id} and match_goal_tbl.member_id = member_tbl.id and match_goal_tbl.status ${status} and match_goal_tbl.status=goal_status_tbl.id group by member_tbl.id`
   const match_goals = await executeQuery(query) ;
-  if (res.length > 0) {
+  if (match_goals.length > 0) {
     for (const member of match_goals) {
       console.log(member) ;
     }
