1ff2f9460704ce86ce7ccc7d157cb29041fd9a78
revemu <128660623+revemu@users.noreply.github.com>
2025-08-23 15:24:43 +0700

    Update query.js

--- query.js
+++ query.js
@@ -584,7 +584,7 @@ async function getMatchWeek(week_id = 0) {
                     bubble.body.contents.push(await queryMatchGoal(match.id, 3)) ;
                 }
                 i++ ;
-                //if (i > 2) break ;
+                if (i > 2) break ;
             }  
             //console.log(JSON.stringify(bubble)) ; 
             bubble.body.contents.push({
@@ -596,6 +596,7 @@ async function getMatchWeek(week_id = 0) {
             for (const table of tables) {
                 bubble.body.contents.push(table) ;
             }
+            console.log(JSON.stringify(bubble)) ;
             return bubble ;
         }
             
