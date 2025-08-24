aee1cb01e3eea7f3d8a25312e12c1be959144b62
revemu <128660623+revemu@users.noreply.github.com>
2025-08-24 09:16:58 +0700

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -152,7 +152,7 @@ async function process_cmd(cmd_str, member, quoteToken) {
             await db.newWeek(next_sat) ;
             const y = next_sat.getFullYear();
             const d = ('0' + next_sat.getDate()).slice(-2);
-            const month = next_sat.toLocaleString('default', { month: 'long' });
+            const month = await db.getMonthName(next_sat.getMonth()+1)
             console.log(next_sat) ;
             msg = `ลงชื่อเตะบอล เสาร์ที่ ${d} ${month} ${y} ได้` ;
             break ;
