6550da5a9cd7397cb230776d729cc13b8d4f990e
revemu <128660623+revemu@users.noreply.github.com>
2025-08-31 14:04:05 +0700

    Update query.js

--- query.js
+++ query.js
@@ -1006,8 +1006,9 @@ async function getMemberWeek2(type = 0) {
               i++ ;
               if (i > 1) break ;
             }
+            user_json = "{" + user_json + "}" ;
             console.log(user_json.replace(/\s/g, "")) ;
-            sub = JSON.parse(user_json.replace(/\s/g, ""))
+            sub = JSON.parse(user_json.replace(/\s/g, "")) 
             console.log(`player: ${player} reserve: ${reserve} goal: ${goal}`) ;
             let str = header + body ;
             header = `+${player}` ;
