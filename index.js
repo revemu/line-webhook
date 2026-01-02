ad2229e3395cf31af40433bc3a6c2a4c93f7d2d3
revemu <128660623+revemu@users.noreply.github.com>
2026-01-02 19:28:00 +0700

    feat: remove user ID restriction from debt list display.

--- index.js
+++ index.js
@@ -518,7 +518,7 @@ async function handleMessage(event) {
                     let debt_count = 0;
                     let proceed = false;
                     [debt_str, sub, debt_count, proceed] = await db.getDebtList(0)
-                    if (proceed && debt_count > 0 && member[0].id == 14) {
+                    if (proceed && debt_count > 0) {
                         let replyMessages = {
                             type: 'textV2',
                             text: debt_str,
