56db057c8b4809faaad29162f62631e4a658f996
revemu <128660623+revemu@users.noreply.github.com>
2026-01-02 19:23:42 +0700

    debug: log source group ID

--- index.js
+++ index.js
@@ -510,6 +510,7 @@ async function handleMessage(event) {
                 //const dow = date.getDay();
                 const h = date.getHours();
                 console.log(`member_id: ${member[0].id} hr: ${h}`);
+                console.log(`source.groupId: ${source.groupId}`);
                 if (h > 12 && h < 22 && source.groupId) {
                     let debt_str = "";
                     let sub = {};
