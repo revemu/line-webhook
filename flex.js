7fe16900edd8abaa7c52845ece20bf065af58cd0
revemu <128660623+revemu@users.noreply.github.com>
2026-08-29 16:08:18 +0700

    style: increase flex component dimensions to 48px

--- flex.js
+++ flex.js
@@ -1004,8 +1004,8 @@ function buildLiveFlex(matchInfo, theme) {
           {
             type: 'box',
             layout: 'vertical',
-            width: '24px',
-            height: '24px',
+            width: '48px',
+            height: '48px',
             flex: 0,
             justifyContent: 'center',
             alignItems: 'center',
