cad02f1d1492ec4a0e1350c42907322220dd711f
revemu <128660623+revemu@users.noreply.github.com>
2026-08-31 15:33:35 +0700

    style: reduce icon size dimensions in flex message layout

--- flex.js
+++ flex.js
@@ -4070,8 +4070,8 @@ function buildMvpListFlex(mvpData, theme) {
         titleContents.push({
           type: 'box',
           layout: 'vertical',
-          width: '28px',
-          height: '28px',
+          width: '22px',
+          height: '22px',
           flex: 0,
           contents: [
             {
